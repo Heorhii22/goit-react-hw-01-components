6d27e956ca4a44f726dfcfcd01385de349f4a69c
George Artemov <113346839+Heorhii22@users.noreply.github.com>
2023-04-06 11:25:15 +0300

    start

--- src/index.js
+++ src/index.js
@@ -2,9 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import userData from 'path/to/user.json';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const user = {
+  username: 'Jacques Gluke',
+  tag: 'jgluke',
+  location: 'Ocho Rios, Jamaica',
+  avatar: 'https://cdn-icons-png.flaticon.com/512/2922/2922506.png',
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308,
+  },
+};
+
+const Profile = () => (
+  <div className="profile">
+    <div className="description">
+      <img src={user.avatar} alt="User avatar" className="avatar" />
+      <p className="name">{user.username}</p>
+      <p className="tag">@{user.tag}</p>
+      <p className="location">{user.location}</p>
+    </div>
+
+    <ul className="stats">
+      <li>
+        <span className="label">Followers</span>
+        <span className="quantity">{user.stats.followers}</span>
+      </li>
+      <li>
+        <span className="label">Views</span>
+        <span className="quantity">{user.stats.views}</span>
+      </li>
+      <li>
+        <span className="label">Likes</span>
+        <span className="quantity">{user.stats.likes}</span>
+      </li>
+    </ul>
+  </div>
 );
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Profile />);
